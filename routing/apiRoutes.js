801475f18e357ab19f94611fdc9dc8061cfcd6dd
Marco Polo <laurenlapoint@Laurens-MacBook-Pro.local>
2018-10-02 18:32:45 -0400

    form validation in html file

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -6,7 +6,6 @@ const friendData = require("../app/data/friends");
 module.exports = function(app){
     app.get("/api/friends", function(req, res){
         res.json(friendData);
-        console.log(friendData);
     });
 }
 
