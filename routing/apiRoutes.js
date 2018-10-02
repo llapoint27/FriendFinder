9f175ac90e04e00ec2cc09c29f8600e0454fc625
Marco Polo <laurenlapoint@Laurens-MacBook-Pro.local>
2018-10-02 17:15:02 -0400

    adding reset.css

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,8 +1,16 @@
+const path = require("path");
 
-const friendData = require("../data/friends");
+//Import the list of friend entries
+const friendData = require("../app/data/friends");
 
 module.exports = function(app){
     app.get("/api/friends", function(req, res){
         res.json(friendData);
+        console.log(friendData);
     });
 }
+
+// app.post("/api/friends", function(req, res){
+
+
+// });
--- server.js
+++ server.js
@@ -7,18 +7,18 @@ const path = require("path");
 const app = express();
 
 //Defining 3000 port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 //Allows the Express app to handle parsing the data within a body
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //Creates the routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+  console.log("FriendFinder App listening on PORT: " + PORT);
 });
